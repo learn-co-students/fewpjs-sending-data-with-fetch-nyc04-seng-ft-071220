f8e4805aecc21c92e74bfe242c8322ea4cb6d2c1
Dfelix02 <david.felix02@hotmail.com>
2020-09-17 19:53:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+
+
+
+// fetch("http://localhost:3000/dogs", {
+//     method: "POST",
+//     headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// })
+//     .then(res=>res.json())
+//     .then(dogObj=>{
+
+//     });
+
+// let theBody = document.querySelector("body");
+
+function submitData(name, email){
+    
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+    body: JSON.stringify({
+        name: name,
+        email: email
+    })
+    })
+        .then(res => res.json())
+        .then(userObj => {
+
+            // let newUserP = document.createElement("p");
+            // newUserP.innerText = userObj["id"];
+            // theBody.append(newUserP);
+
+            document.body.innerHTML = userObj["id"];
+    })
+        .catch(error =>{
+
+            // let errorP = document.createElement("p");
+            // errorP.innerText = error.message;
+            // document.body.append(errorP);
+
+            document.body.innerHTML = error.message;
+        })
+}
+
