6a183c77307298ca875b429870130538245e4b0f
wilsonng21 <wilsongch92@gmail.com>
2020-09-20 09:19:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+
+function submitData(userName, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: email
+        })
+    })
+    .then( response => response.json())
+    .then( userObj => {
+        document.body.innerHTML = userObj.id
+    })
+    .catch( error => 
+        document.body.innerHTML = error.message
+    )
+}
