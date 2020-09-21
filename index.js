8fadc72796317220b4d9ce9318063fb0ca13f462
nisayana <nimaevasb@gmail.com>
2020-09-21 10:13:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users",{
+        // evt.preventDefault()
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+
+    .then(res => res.json())
+    .then((newObj) => {
+        console.log(newObj)
+        document.body.innerHTML = newObj["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+
+}
